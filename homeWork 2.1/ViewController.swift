959a76823cfa839fc7cb7d35d57adbd34492c4c9
Andrey <glinkin.a.o@yandex.ru>
2020-12-03 03:20:37 +1000

    finish hw2.1 =(

--- homeWork 2.1/ViewController.swift
+++ homeWork 2.1/ViewController.swift
@@ -2,18 +2,40 @@
 //  ViewController.swift
 //  homeWork 2.1
 //
-//  Created by Andrey Glinkin on 02.12.2020.
+//  Created by Andrey Glinkin on 03.12.2020.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var redColor: UIView!
+    @IBOutlet var yellowColor: UIView!
+    @IBOutlet var greenColor: UIView!
+    
+    @IBOutlet var startButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        redColor.alpha = 0.3
+        yellowColor.alpha = 0.3
+        greenColor.alpha = 0.3
+        redColor.layer.cornerRadius = 50
+        yellowColor.layer.cornerRadius = 50
+        greenColor.layer.cornerRadius = 50
+        startButton.layer.cornerRadius = 10
 
+    }
+    @IBAction func pushButton(_ sender: Any) {
+        redColor.alpha = 1
+        startButton.setTitle("NEXT", for: .normal)
+        if redColor.alpha == 1 {
+            redColor.alpha = 0.3
+            yellowColor.alpha = 1
+            
+        
+        }
+    }
+    
 
 }
-
