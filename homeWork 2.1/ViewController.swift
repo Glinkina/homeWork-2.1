18a0dafc92b00c337ebc17721f370832a2035998
Andrey <glinkin.a.o@yandex.ru>
2020-12-04 22:45:54 +1000

    Finish traffic light!

--- homeWork 2.1/ViewController.swift
+++ homeWork 2.1/ViewController.swift
@@ -13,27 +13,34 @@ class ViewController: UIViewController {
     @IBOutlet var greenColor: UIView!
     
     @IBOutlet var startButton: UIButton!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.layer.contents = #imageLiteral(resourceName: "qwerty.jpg").cgImage
         
         redColor.alpha = 0.3
         yellowColor.alpha = 0.3
         greenColor.alpha = 0.3
-        redColor.layer.cornerRadius = 50
-        yellowColor.layer.cornerRadius = 50
-        greenColor.layer.cornerRadius = 50
+        redColor.layer.cornerRadius = 85
+        yellowColor.layer.cornerRadius = 85
+        greenColor.layer.cornerRadius = 85
         startButton.layer.cornerRadius = 10
-
     }
+    
     @IBAction func pushButton(_ sender: Any) {
-        redColor.alpha = 1
+        if startButton.titleLabel?.text == "START" {
+            redColor.alpha = 1
         startButton.setTitle("NEXT", for: .normal)
-        if redColor.alpha == 1 {
+        } else if redColor.alpha == 1 {
             redColor.alpha = 0.3
             yellowColor.alpha = 1
-            
-        
+        } else if yellowColor.alpha == 1 {
+            yellowColor.alpha = 0.3
+            greenColor.alpha = 1
+        } else if greenColor.alpha == 1 {
+            greenColor.alpha = 0.3
+            redColor.alpha = 1
         }
     }
     
